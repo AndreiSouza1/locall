d4f321f98ebbb0c8c9dd1fa9cba3a8b809184ff5
João Vitor Neto Pietro <joao@atplus.com.br>
2025-05-17 18:10:45 -0300

    Revert "Adicionado PopUp inicial"
    This reverts commit 4a8a482ca8a8c80755e36720a7f1593ddd2cfe00.

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,11 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Merchants from './pages/Merchants';
-import WelcomePopup from './components/WelcomePopup';
 
 function App() {
   return (
     <Router>
       <div className="h-screen w-screen overflow-hidden">
-        <WelcomePopup />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/merchants" element={<Merchants />} />
